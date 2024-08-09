1a6d94b2aa07358dce70b5c8c4d58bf545645372
gonzaloqh <gonzalo.qh.94@gmail.com>
2024-08-09 15:59:34 -0500

    Feat: Fix

--- functions/transformWebhook.js
+++ functions/transformWebhook.js
@@ -1,26 +1,21 @@
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 exports.handler = async (event) => {
   try {
-    // Parsear el cuerpo del POST
     const data = JSON.parse(event.body);
 
-    // Transformar el JSON para Google Chat
     const googleChatPayload = {
       text: `Nuevo mensaje: ${data.message}`
     };
 
-    // URL del webhook de Google Chat
     const googleChatWebhookUrl = 'https://chat.googleapis.com/v1/spaces/XXX/messages?key=XXX&token=XXX';
 
-    // Enviar el webhook a Google Chat
     const response = await fetch(googleChatWebhookUrl, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(googleChatPayload),
     });
 
-    // Verificar el resultado del envío
     if (!response.ok) {
       throw new Error(`Error al enviar el webhook a Google Chat: ${response.statusText}`);
     }
