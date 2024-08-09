8e4cbea3246b3b60dc58d3abd9de019938588afd
gonzaloqh <gonzalo.qh.94@gmail.com>
2024-08-09 16:17:01 -0500

    Feat: Card setting

--- functions/transformWebhook.js
+++ functions/transformWebhook.js
@@ -4,11 +4,78 @@ exports.handler = async (event) => {
   try {
     const data = JSON.parse(event.body);
 
+    // Definimos las variables comunes que usaremos en ambos casos
+    const buildNumber = data.buildInfo.id;
+    const buildState = data.state;
+    const creationDate = data.created;
+    const finishedDate = data.finished;
+    const appName = data.app.name;
+    const mergeBy = data.commit.user.name;
+    const downloadLinks = data.artifacts?.map(artifact => {
+      return {
+        "text": artifact.name,
+        "url": artifact.url
+      };
+    }) || [];
+
+    // Creación del payload para Google Chat con "cards"
     const googleChatPayload = {
-      text: `Nuevo mensaje: ${data.message}`
+      cards: [
+        {
+          header: {
+            title: `Build ${buildNumber} - ${appName}`,
+            subtitle: `Estado: ${buildState.toUpperCase()}`,
+            imageUrl: data.app.icon || data.commit.user.picture,
+            imageStyle: "AVATAR"
+          },
+          sections: [
+            {
+              widgets: [
+                {
+                  keyValue: {
+                    topLabel: "Fecha de Creación",
+                    content: new Date(creationDate).toLocaleString(),
+                    icon: "CLOCK"
+                  }
+                },
+                {
+                  keyValue: {
+                    topLabel: "Fecha de Finalización",
+                    content: new Date(finishedDate).toLocaleString(),
+                    icon: "CLOCK"
+                  }
+                },
+                {
+                  keyValue: {
+                    topLabel: "Mergeado por",
+                    content: mergeBy,
+                    icon: "PERSON"
+                  }
+                }
+              ]
+            },
+            ...(downloadLinks.length > 0 ? [{
+              widgets: [
+                {
+                  buttons: downloadLinks.map(link => ({
+                    textButton: {
+                      text: link.text,
+                      onClick: {
+                        openLink: {
+                          url: link.url
+                        }
+                      }
+                    }
+                  }))
+                }
+              ]
+            }] : [])
+          ]
+        }
+      ]
     };
 
-    const googleChatWebhookUrl = 'https://chat.googleapis.com/v1/spaces/XXX/messages?key=XXX&token=XXX';
+    const googleChatWebhookUrl = 'https://chat.googleapis.com/v1/spaces/AAAAqTDcwSQ/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=0mMIFS2n1trg5Pgm9ftzJ4g2UID44t8VoRykxreN9cY';
 
     const response = await fetch(googleChatWebhookUrl, {
       method: 'POST',
